4052e0c60966c252bf86447f675736532646aa3a
VinnyVNS <vinisouzavns@gmail.com>
2026-08-28 16:30:12 -0300

    update

--- scripts/exercicio-13.js
+++ scripts/exercicio-13.js
@@ -1,11 +1,14 @@
+function gerarTabuada(numero) {
+    for(let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
+}
+
 function mostrarExer13() {
     let num = Number(prompt('Digite um número:'));
 
-    for(let i = 1; i <= 10; i++) {
-        let resultado = num * i;
-
-        console.log(`${num} x ${i} = ${resultado}`);
-    }
+    gerarTabuada(num);
 }
 
 export default mostrarExer13;
